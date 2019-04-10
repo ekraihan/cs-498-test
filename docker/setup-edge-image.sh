aa4a33ae588051d691708c179dc71bd1e6980dd7
ekraihan <ekraihan@iupui.edu>
2019-04-10 00:04:05 -0400

    restructure

---
+++ docker/setup-edge-image.sh
@@ -0,0 +1,17 @@
+
+# Install dependencies
+yum update -y
+yum install -y which vim tar gzip java-1.8.0-openjdk.x86_64 procps bzip2 gcc hostname
+
+echo "Installing hadoop"
+curl -s "http://apache.osuosl.org/hadoop/common/hadoop-3.2.0/hadoop-3.2.0.tar.gz" | tar -xz -C /usr/local/
+
+echo "Installing hbase"
+curl -s "http://archive.apache.org/dist/hbase/1.4.9/hbase-1.4.9-bin.tar.gz" | tar -xz -C /usr/local/
+
+echo "Installing miniconda"
+curl -s "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" > /tmp/install-miniconda.sh
+bash /tmp/install-miniconda.sh -b -f -p /usr/lib/miniconda
+
+echo y | conda install cython
+pip install happybase flask
